0385133fac6ae8c14d1cbf2fbc71331d800cd25f
AlexJamesDean <lead@alexjamesdean.co.uk>
2025-11-19 09:34:17 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'AJD Development'
+author 'AlexJamesDean'
 description 'Promotional repository for AJD Scavenge. Purchase the full script on Tebex.'
 version '1.0.0'
 
 -- No client or server scripts are shipped here.
 -- Grab the complete resource with all source files from https://ajd.tebex.io/.
+
 
