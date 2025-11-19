ca05198d95758a3f7ce1a7becdff9423e999a445
AlexJamesDean <lead@alexjamesdean.co.uk>
2025-11-19 09:33:46 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'AJD Development'
+description 'Promotional repository for AJD Scavenge. Purchase the full script on Tebex.'
+version '1.0.0'
+
+-- No client or server scripts are shipped here.
+-- Grab the complete resource with all source files from https://ajd.tebex.io/.
+
